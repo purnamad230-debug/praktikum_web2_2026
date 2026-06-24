30c6efc8808ecd54e36bbda35bd56a3775c0b131
Dewipurnama <purnamad230@gmail.com>
2026-06-24 13:16:03 +0700

    menentukan relasi dan casts pada model borrow

---
+++ app/Models/Borrow.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Borrow extends Model
+{
+    protected $fillable = [
+        'book_id',
+        'user_id',
+        'date_start',
+        'date_end',
+    ];
+    protected function casts(): array
+    {
+        return [
+            'date_start' => 'date',
+            'date_end' => 'date',
+        ];
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+}
