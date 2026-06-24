220e16fb874c46dc5cf76076e9955e46104276e7
Dewipurnama <purnamad230@gmail.com>
2026-06-24 13:22:11 +0700

    menentukan relasi dan casts pada model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,17 +16,28 @@ class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
-
+    
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+    ];
     /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
      */
+    // casts mengubah tipe data dari kolom yang ada di database, misal kolom email_verified_at bertipe datetime, dipakai untuk tanggal dan waktu, sedangkan kolom password diubah menjadi hashed, dipakai untuk enkripsi password.
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function borrow()
+    {
+        return $this->hasMany(Borrow::class);
     }
 }
